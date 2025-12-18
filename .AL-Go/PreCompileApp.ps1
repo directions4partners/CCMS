2ff6f53631d476bdfe1a856d31db66788bf7ba32
Freddy Kristiansen <freddy@directions4partners.com>
2025-12-18 06:36:31 +0100

    remove dump

--- .AL-Go/PreCompileApp.ps1
+++ .AL-Go/PreCompileApp.ps1
@@ -4,9 +4,5 @@ Param(
 )
 
 if ($appType -eq 'app') {
-    Write-Host "Checking translations for app: $appFilePath"
+    $compilationParams.Value["features"] += "TranslationFile"
 }
-else {
-    return
-}
-$compilationParams.Value["features"] += "TranslationFile"
