de4b66e4c1bfdfe80e47ec934b98d92ce1b6c214
Freddy Kristiansen <fkr@freddy.dk>
2025-12-20 13:15:19 +0100

    write seperately

--- .AL-Go/PreCompileApp.ps1
+++ .AL-Go/PreCompileApp.ps1
@@ -5,11 +5,7 @@ Param(
 
 Get-ChildItem -path (Join-Path $compilationParams.Value.appProjectFolder "Translations") -Filter *.xlf -Recurse | ForEach-Object {
     Write-Host "Found translation file: $($_.FullName)"
-    Write-Host "---------------------------------------------- BEFORE -----------------------------------------------"
-    Get-Content -Path $_.FullName | Out-Host
     # Remove NAB tags before compiling
-    Get-Content -Path $_.FullName | ForEach-Object { $_ -replace '\[NAB:[^<>\[\]]*\]','' } | Set-Content -Path $_.FullName
-    Write-Host "---------------------------------------------- AFTER -----------------------------------------------"
-    Get-Content -Path $_.FullName | Out-Host
-    Write-Host "---------------------------------------------- END -----------------------------------------------"
+    $lines = Get-Content -Path $_.FullName -Encoding UTF8 | ForEach-Object { $_ -replace '\[NAB:[^<>\[\]]*\]','' }
+    Set-Content -Path $_.FullName -Value $lines -Encoding UTF8
 }
